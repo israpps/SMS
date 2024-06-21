07d76d17d170cdb51ee38a20530631f9751fd408
Matías Israelson <57065102+israpps@users.noreply.github.com>
2024-06-21 19:25:50 -0300

    patch fileio

--- src/SMS_IOP.c
+++ src/SMS_IOP.c
@@ -192,6 +192,7 @@ void SMS_IOPReset ( int afExit ) {
  _slib_cur_exp_lib_list.head = NULL;
  sbv_patch_enable_lmb           ();
  sbv_patch_disable_prefix_check ();
+ sbv_patch_fileio();
 
  //while(!SifIopReset(s_iop_image, 0)){};
 
